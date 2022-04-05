9a61374ce4736e8a4bc0f8996b6c17c08df5f8b7
Victor Pena <victorpenawow@gmail.com>
2022-04-05 14:55:36 +0200

    Modulo db añadido

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contadores/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ContadorModule } from './contadores/contador.module';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],exports:[
+    MainPageComponent
+  ]
+})
+export class DbzModule { }
