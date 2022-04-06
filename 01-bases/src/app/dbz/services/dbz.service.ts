b2b0e534638aad187244ded8dac8ffff8dc53e07
Victor Pena <victorpenawow@gmail.com>
2022-04-06 12:40:15 +0200

    Proyecto de dbz arreglado, era la _ de la variable privada

--- 01-bases/src/app/dbz/services/dbz.service.ts
+++ 01-bases/src/app/dbz/services/dbz.service.ts
@@ -13,11 +13,11 @@ export class DbzService{
           nombre:'Vegeta',
           poder: 7500
         }
-      ]
-
+      ];
+      
       get personajes(): Personaje[]{
           //El [...] rompe la referencia con la variable, es como decir separa la referencia con la variable this. de aqui y generate una copia en donde se invoque el get. Esto no es necesario en java, es sobre todo de javascript pero por buena practica se pone.
-        return [...this.personajes];
+        return [...this._personajes];
       }
 
     constructor(){
